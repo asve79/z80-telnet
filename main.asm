8b869d855fe74c686509dd3d1b3049e3b4ed212f
Anton <asve@ae-nest.com>
2018-12-28 08:49:04 +0300

    Day#7. Refactoring main procedure

--- main.asm
+++ main.asm
@@ -16,12 +16,35 @@ PROG
 mloop   CALL	check_rcv
 	CALL    spkeyb.CONINW	;main loop entry
 	JZ	mloop		;wait a press key
-	cp	01Dh
-	jp	z,exit		;if SS+Q pressed, exit
+	PUSH 	AF
+	_idcmdmode		;if comman mode on go to cmdmodeproc
+	JZ	cmdmodeproc
+	;process terminal mode
+	POP	AF
+	CP	01Dh
+	JZ	exit		;if SS+Q pressed, exit
 	CP	01Ch		;if Ss+W pressed - terminal command
-	JP	NZ,m_lab4	; ------>
-	_iscmdmode		;check terminal command mode (0 - tty 1 - command mode)
-	JP	Z, m_lab3
+	JZ	opencmdnode	
+	CP	#7F		;//delete key pressed
+	JZ	delsymtermmode	
+	CP	13		;//enter key pressed
+	JZ	enterkeytermmode
+	CALL	puttocmdbufer	;//put char to command bufer and print
+	JP	mloop
+cmdmodeproc ;process comman mode
+	POP	AF
+	CP	01Dh
+	JZ	closecmdmode	;if SS+Q pressed, exit
+	CP	01Ch		;if Ss+W pressed - terminal command
+	JZ	closecmdnode
+	CP	#7F		;//delete key pressed
+	JZ	delsymcmdmode	
+	CP	13		;//enter key pressed
+	JP	enterkeycmdmode
+	CALL	puttotermbufer	;//put char to terminal bufer and print
+	JP	mloop
+
+opencmdmode ;open commend window
 	LD	A,1		;if terminal command mode is off
 	LD	(termcmd),A	;turn on termianl mode
 	_cur_off
@@ -29,21 +52,24 @@ mloop   CALL	check_rcv
 	_prints	cmd_bufer	;print content of command buffer
 	_cur_on
 	JP	mloop
-m_lab3	_cur_off		;close the commend window
+;----
+closecmdmode ;close the commend window
+	_cur_off		
 	_endw
 	XOR	A
 	LD	(termcmd),A
 	_cur_on
 	JP	mloop
-m_lab4	CP	#7F		;//delete key pressed
-	JNZ	m_lab13		;//if not go to m_lab11
-	_istermcommand
-	JZ	m_lab11		;//if it is temminal command
-	_findzero cmd_bufer	;//get ptr in command bufer
-	JR	m_lab14
-m_lab11	_findzero inp_bufer	;//get ptr in input buffer
-m_lab14	OR	A
-	JZ	m_lab13		;//if nothing in bufer
+;-----
+delsymcmdmode	;delete symbol in command bufer
+	LD	HL,cmd_bufer
+	JR	delsymproc
+delsymtermmode	;delete symbol in terminal mode
+	LD	HL,inp_bufer
+delsymproc	;delete symbol main proc
+	_findzero inp_bufer	;//get ptr on last symbol+1 in buffer
+	OR	A
+	JZ	mloop		;//if nothing in bufer (length=0)
 	DEC	HL
 	XOR	A
 	LD	(HL),A		;//erase symbol
@@ -54,45 +80,17 @@ m_lab14	OR	A
 	LD	A,8		;//left again
 	_printc
 	JP	mloop
-m_lab13	PUSH 	AF		;//check connected
+;----
+enterkeycmdmode	;enter key pressed in command window. execute command if it exists
+	_isopencommand cmd_bufer,eccm1	;//'open'  command
+	_isclosecommand cmd_bufer,eccm1 ;//'close' command
+	_ishelpcommand cmd_bufer,eccm1	;//'help' command
+eccm1	_fillzero cmd_bufer, 100	;clear command buffer
+	jP 	mloop
+;----
+enterkeycmdmode	;enter key pressed in terminal window
 	_isconnected
-	JNZ	m_lab6
-	LD	A,(conn_descr)
-	CP	#FF		;//check descriptor
-	JZ	m_lab6
-	LD	A,(termcmd)
-	OR	A		;check terminal command mode
-	JP	NZ, m_lab6
-	_findzero inp_bufer	;/put char to output buffer
-	POP	AF
-	LD	(HL),A
-	PUSH	AF
-m_lab6	POP	AF
-;	_cur_off
-	call	wind.PRINTC	;out character
-	CP	13		;check for press enter
-	JP	NZ, m_lab5
-	_istermcommand
-;	LD	A,(termcmd)	
-;	OR	A
-	JZ	m_lab8		;if it is not terminal command
-	_isopencommand cmd_bufer,m_lab2
-	_isclosecommand cmd_bufer,m_lab2
-	_ishelpcommand cmd_bufer,m_lab2
-m_lab2	_fillzero cmd_bufer, 100	;clear command buffer
-	jp 	mloop
-m_lab5	LD	HL,termcmd	;write terminal command to bufer
-	PUSH 	AF
-	LD	A,(HL)
-	OR	A
-	JNZ 	mloop
-	_findzero cmd_bufer
-	POP	AF
-	LD	(HL),A
-	JP	mloop
-m_lab8	LD	A,(connected)	;//if ENTER pressed in terminal
-	OR	A
-	JZ	m_lab9		;//if not connected
+	JNZ	ekcm_nc		;//if not connected
 	_findzero inp_bufer
 	LD	C,A
 	LD	A,13		;/add 13 code for <CR><LF> EOL command
@@ -105,21 +103,32 @@ m_lab8	LD	A,(connected)	;//if ENTER pressed in terminal
 	LD	A,(conn_descr)
 	CALL	sockets.send	;//send buffer content
 	OR	A
-	JZ	m_lab7
-	_printw wnd_status
+	JZ	ekcm_ok
+	_printw wnd_status	;//if error while send data
 	LD	A,'E'		;//error status TODO: close connection
 	_printc
 	_closew
 	_cur_on
-	JP	m_lab9
-m_lab7	_printw wnd_status	;//success status
+	JP	ekcm_nc
+ekcm_ok	_printw wnd_status	;//success status
 	LD	A,'#'
 	_printc
 	_closew
 	_cur_on
-m_lab9	_fillzero inp_bufer, 255
+ekcm_nc	_fillzero inp_bufer,255
 	JP	mloop
-
+;- routine -
+puttocmdbufer	;put symbol in command bufer
+	PUSH	AF
+	_findzero cmd_bufer
+	JR	puttobufer
+puttotermbufer	;put symbol to terminal bufer
+	_findzero inp_bufer
+puttobufer	;main procedure for put to bufer;TODO make insert mode with shift content
+	POP	AF
+	LD	(HL),A
+	call	wind.PRINTC	;out character
+	RET
 
 exit	_cur_off		;TOOD: close connection if needed
 	_closew
